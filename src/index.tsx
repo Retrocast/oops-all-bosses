0f9d1543b5ed047feef4c0f737c3d9e8b848884b
Retrocast <retr0cast@proton.me>
2025-02-14 15:18:14 -0600

    chore: set up mantine boilerplate

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import { render } from "preact";
 
-import "./style.css";
+import "@mantine/core/styles.css";
+import {
+  Center,
+  DEFAULT_THEME,
+  MantineProvider,
+  mergeMantineTheme,
+  Title,
+} from "@mantine/core";
+
+const theme = mergeMantineTheme(DEFAULT_THEME, {
+  primaryColor: "red",
+  cursorType: "pointer",
+  fontFamily:
+    'ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
+});
 
 export function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <MantineProvider forceColorScheme="dark" theme={theme}>
+      <Center h="100vh" w="100vw" style={{ backgroundColor: "black" }}>
+        <Title order={1} c="red.7">
+          Hello, world!
+        </Title>
+      </Center>
+    </MantineProvider>
   );
 }
 
