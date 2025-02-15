89715bf84845c2b2c5fcb423d58475a71dd81b8a
Retrocast <retr0cast@proton.me>
2025-02-14 19:08:49 -0600

    feat: add basic error handling

--- src/editor.tsx
+++ src/editor.tsx
@@ -1,9 +1,10 @@
-import { Button, FileButton, Text } from "@mantine/core";
+import { Anchor, Button, FileButton, Text, Textarea } from "@mantine/core";
 import { IconDownload, IconUpload } from "@tabler/icons-react";
 import { useState } from "preact/hooks";
 
 export function Editor() {
   let [file, setFile] = useState<File>();
+  let [error, setError] = useState<string | Error>();
   return (
     <>
       <FileButton accept=".gwsave" onChange={setFile}>
@@ -17,6 +18,36 @@ export function Editor() {
           </Button>
         )}
       </FileButton>
+      {error && (
+        <>
+          <Text c="red.7">Error!</Text>
+          {typeof error == "string" ? (
+            <Text c="red.7">{error}</Text>
+          ) : (
+            <Textarea
+              disabled
+              variant="unstyled"
+              value={error.message + "\n" + error.stack}
+              w="100%"
+              autosize
+              minRows={5}
+            ></Textarea>
+          )}
+          {typeof error != "string" && (
+            <Text c="red.7">
+              You should probably contact author about it -{" "}
+              <Text span c="red">
+                @retrcast
+              </Text>{" "}
+              on Discord or{" "}
+              <Anchor href="https://www.reddit.com/user/Comfortable-Roll-490/">
+                u/Comfortable-Roll-490/
+              </Anchor>{" "}
+              on Reddit
+            </Text>
+          )}
+        </>
+      )}
       {file && (
         <>
           <Text>
@@ -31,6 +62,7 @@ export function Editor() {
               link.href = URL.createObjectURL(
                 new Blob([await file.arrayBuffer()])
               );
+              setError(new Error("Not implemented"));
               link.click();
             }}
           >
